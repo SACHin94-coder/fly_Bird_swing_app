9e92932fb8fb504951faba5ce987b882cd62681c
sachinsahani <sachinsamar179@gmail.com>
2026-01-31 14:20:25 +0530

    added the image to background of the app

--- flybird.java
+++ flybird.java
@@ -7,8 +7,28 @@ public class flybird extends JPanel {
     int boardWidth = 360;
     int boardHeight = 740;
 
+    // images
+    Image background;
+    Image topPipe;
+    Image lowPipe;
+    Image bird;
+
     flybird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground((Color.blue));
+
+        background = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        topPipe = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        lowPipe = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+        bird = new ImageIcon(getClass().getResource("./bird.png")).getImage();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(background, 0, 0, boardWidth, boardHeight, null);
     }
 }
